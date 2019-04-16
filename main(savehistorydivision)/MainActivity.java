5a806314c5f2da7642802e09cf6cabcd7eb793f8
trinhhuuhoang060 <37512856+trinhhuuhoang060@users.noreply.github.com>
2019-04-16 23:26:43 +0700

    Add files via upload

---
+++ main(savehistorydivision)/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.hoangle.savehistorydivision;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edta,edtb;
+    TextView txtresult;
+    Button button;
+    ListView listView;
+    ArrayList<String> arrayList;
+    ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Init();
+        arrayAdapter = new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,arrayList);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                arrayList.remove(position);
+                arrayAdapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edta.getText().toString().isEmpty() || edtb.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Moi ban nhap day du", Toast.LENGTH_SHORT).show();
+                }else {
+                    double a = Integer.parseInt(edta.getText().toString());
+                    double b = Integer.parseInt(edtb.getText().toString());
+                    double c = a/b;
+                    txtresult.setText(a + " / " + b +" = " +c);
+                    arrayList.add(a + " / " + b +" = " +c);
+                    arrayAdapter.notifyDataSetChanged();
+                    edta.setText("");
+                    edtb.setText("");
+                }
+            }
+        });
+
+    }
+
+    private void Init() {
+        arrayList = new ArrayList<>();
+        edta = (EditText)findViewById(R.id.edta);
+        edtb = (EditText)findViewById(R.id.edtb);
+        txtresult = (TextView)findViewById(R.id.txtresult);
+        button = (Button)findViewById(R.id.button);
+        listView = (ListView)findViewById(R.id.listview);
+    }
+}
